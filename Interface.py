c370a0328e4bb04f225624428e44625030b0b32d
ParadoxZero <sidhin.thomas@gmail.com>
2016-01-21 23:26:17 +0530

    Initial display function: basic

---
+++ Interface.py
@@ -0,0 +1,27 @@
+#
+#
+#
+# Project name : Lisa
+#
+# A simple chat-Bot.
+#
+#
+# 
+import time
+import sys
+
+
+
+class Lisa :
+	
+	def display(self,data):
+		for d in data:
+			sys.stdout.write(d)
+			sys.stdout.flush()
+			time.sleep(0.1el)
+		print ""
+
+if __name__ == "__main__":
+	l = Lisa()
+	d = input("")
+	l.display(d)
