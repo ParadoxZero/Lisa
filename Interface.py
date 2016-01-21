2618b8dcc06b1ef542d06bf1c05903e3ad945fae
ParadoxZero <sidhin.thomas@gmail.com>
2016-01-22 02:55:30 +0530

    work in progress

--- Interface.py
+++ Interface.py
@@ -13,15 +13,16 @@ import sys
 
 
 class Lisa :
+	_replyEngine = replyEngine()
 	
 	def display(self,data):
 		for d in data:
 			sys.stdout.write(d)
 			sys.stdout.flush()
-			time.sleep(0.1el)
+			time.sleep(0.1)
 		print ""
 
 if __name__ == "__main__":
 	l = Lisa()
-	d = input("")
+	d = raw_input("")
 	l.display(d)
